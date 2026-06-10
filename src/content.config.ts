aebe53b8bf7b945c1831ab8d6e3417114ebc584b
thomaspeterkueper <thomas.peter.kueper@gmail.com>
2026-06-10 12:44:23 +0200

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -32,7 +32,7 @@ const kontrakomologie = defineCollection({
     title: z.string(),
     subtitle: z.string().optional(),
     description: z.string().optional(),
-    kategorie: z.enum(['pop', 'klassik', 'jazz', 'grundlagen', 'eigenes-werk']).optional(),
+    kategorie: z.enum(['pop', 'klassik', 'jazz', 'grundlagen','literatur', 'eigenes-werk', 'neue-musik']).optional(),
     richtung: z.enum(['text-zu-musik', 'musik-zu-text']).optional(),
     kuenstler: z.string().optional(),
     werk: z.string().optional(),
